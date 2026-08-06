1ca1e1da5dcd5d22b7167d77b1931c2b11d3ab7d
Fauzan Fauzia Fauzi <fauzanfauziafauzi@gmail.com>
2026-08-06 14:10:14 +0700

    feat: implement keyboard shortcut listener for search trigger

--- 02-Navbar_Github/assets/js/script.js
+++ 02-Navbar_Github/assets/js/script.js
@@ -8,3 +8,21 @@ hamburgerBtn.addEventListener("click", () => {
   // Memberi/mencabut class active sebagai penanda interaksi
   hamburgerBtn.classList.toggle("active");
 });
+
+// 3. SELECT SEARCH BUTTON
+const searchBtn = document.querySelector(".gh-search-btn");
+
+// 4. EVENT LISTENER KEYBOARD SHORTCUT (Tekan tombol '/')
+document.addEventListener("keydown", (event) => {
+  // Cek apakah tombol yang ditekan adalah '/' dan user sedang tidak di input teks
+  if (event.key === "/" && document.activeElement.tagName !== "INPUT") {
+    event.preventDefault(); // Mencegah karakter '/' ketik sembarangan
+    console.log("Shortcut / berhasil dipicu!");
+    searchBtn.click(); // Simulasi klik tombol search
+  }
+});
+
+// Event klik pada tombol search
+searchBtn.addEventListener("click", () => {
+  alert("Search bar dipicu! Nanti bisa dibuka modal pencariannya di sini.");
+});
